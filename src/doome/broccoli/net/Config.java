9e0bc9c2f5a34e1a5d987669319fd65888daca48
DESKTOP-VSM6BLJ\danah <turbolady36@gmail.com>
2018-05-01 13:40:23 +0900

    d

--- src/doome/broccoli/net/Config.java
+++ src/doome/broccoli/net/Config.java
@@ -7,4 +7,5 @@ public class Config {
 	public static final String KEYWORDS = "닭고기,오리고기";
 	public static final String URL = "http://www.thebroccoli.net";
 	public static final String OG_IMG = "http://www.thebroccoli.net/temp/img/comm/og.jpg";
+	public static final String FAVICON = "http://www.thebroccoli.net/temp/img/comm/favicon.ico"; 
 }
