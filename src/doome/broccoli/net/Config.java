32d65161b2397a67bece3ada465229304cfc0627
Lee <turboguy36@gmail.com>
2018-05-10 12:52:13 +0900

    키워드 추가

--- src/doome/broccoli/net/Config.java
+++ src/doome/broccoli/net/Config.java
@@ -4,7 +4,7 @@ public class Config {
 
 	public static final String TITLE = "두메산골영농조합법인";
 	public static final String DESCRIPTION = "두메산골영농조합 법인 사이트 입니다.";
-	public static final String KEYWORDS = "닭고기,오리고기";
+	public static final String KEYWORDS = "닭고기, 생닭, 삼계, 토종닭, 닭가슴살, 안심, 봉, 윙, 북채, 정육, 장각, 닭다리, 닭날개, 닭안심, 절단육, 오리고기, 생오리, 오리절단, 오리정육, 오리정육슬라이스, 오리슬라이스, 오리훈제, 닭가슴살훈제, 훈제닭가슴살, 훈제오리, 훈제샐러드, 훈제닭가슴살샐러드, 닭가슴살훈제샐러드, 유정란, 맑은란, 계란, 달걀, 인증계란, 바이전주, 바이전북, HACCP, 전라북도, 전주시, 전주, 납품, 배달, 주문, 주문제작, 맞춤제작, 도매, 소매, 온라인, 쇼핑몰, 온라인쇼핑몰, 신선, 깨끗, 친환경, 웰빙";
 	public static final String URL = "http://www.thebroccoli.net";
 	public static final String OG_IMG = "http://www.thebroccoli.net/temp/img/comm/og.jpg";
 	public static final String FAVICON = "http://www.thebroccoli.net/temp/img/comm/favicon.ico";
